317ab0402bec6b4ba0398ec36bc11a456361a336
ladniak <sergeyladnyak7@gmail.com>
2025-11-04 23:38:04 +0200

    add signOut and new pages

--- app/dashboard/components/SideNav.tsx
+++ app/dashboard/components/SideNav.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { signOut } from "next-auth/react";
 
 export default function SideNav() {
     const pathname = usePathname();
@@ -11,21 +12,29 @@ export default function SideNav() {
         { label: "Settings", href: "/dashboard/settings" },
     ];
 
+    const handleSignOut = () => {
+        signOut({ callbackUrl: "/login" })
+    }
+
     return (
-        <nav className="w-64 h-screen bg-gray-100 p-4">
-            <ul className="space-y-3">
-                {navItems.map((item) => (
-                    <li key={item.href}>
-                        <Link
-                            href={item.href}
-                            className={`block p-2 rounded ${pathname === item.href ? "bg-blue-500 text-white" : "text-gray-700"
-                                }`}
-                        >
-                            {item.label}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        </nav>
+        <div>
+            <nav className="w-64 h-screen bg-gray-100 p-4">
+                <ul className="space-y-3">
+                    {navItems.map((item) => (
+                        <li key={item.href}>
+                            <Link
+                                href={item.href}
+                                className={`block p-2 rounded ${pathname === item.href ? "bg-blue-500 text-white" : "text-gray-700"
+                                    }`}
+                            >
+                                {item.label}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+            <button className="" onClick={handleSignOut}>Sign out</button>
+        </div>
+
     );
 }
---
+++ app/dashboard/settings/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+    return (
+        <h1>Setting Page</h1>
+    );
+}
